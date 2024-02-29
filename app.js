e8a3e2002c8022b8bf1c2c2c8eed0df73ac83883
sheraz <sherazkarim12@gmail.com>
2024-02-29 15:49:19 +0500

    commited

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const app = express ();
-
+const app = express();
 
 app.use((req, res, next) => {
     res.status(200).json({
-        Message:'it works!'
+        Message: 'it works!'
     });
+});
 
 module.exports = app;
-
-})
+ xc
